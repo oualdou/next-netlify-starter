f686319ae530e1725fb888ee10080a361917f738
oualdou <anas.oualdou@gmail.com>
2023-05-11 16:53:01 +0200

    etst

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,16 @@ export default function Home() {
      
     
       
-        <h1>Bienvenue sur notre site de dépannage automobile en Île-de-France, ouvert 24h/24 !</h1>
-        <Image
+      <h1>Dépannage automobile - Ile-de-France</h1>
+      <h2>Ouvert 24h/24</h2>
+
+      <Image
         src="public/background.png"
         width={500}
         height={500}
         alt="Picture of the author"
       />
+
         <p>Lorsque vous êtes confronté à une panne de voiture, il est essentiel de pouvoir compter sur un service fiable et rapide. Notre équipe de professionnels expérimentés est là pour vous aider, que vous soyez en pleine nuit, en weekend ou pendant les jours fériés. Nous comprenons que les pannes de voiture peuvent survenir à tout moment, c est pourquoi nous sommes disponibles à toute heure pour vous fournir une assistance immédiate.</p>
         <p>Nos services de dépannage couvrent tous types de pannes, des problèmes de batterie déchargée aux crevaisons, en passant par les pannes mécaniques plus complexes. Nous disposons des équipements nécessaires pour résoudre la plupart des problèmes sur place, vous permettant ainsi de reprendre rapidement la route en toute tranquillité.</p>
         <p>En plus de notre service de dépannage, nous proposons également des services complémentaires tels que le remorquage de véhicules et la fourniture de carburant d urgence. Notre objectif est de vous offrir une solution complète pour tous vos besoins en matière de dépannage automobile.</p>
