2d4784bb7bee4b88d81d0e9318e93dbff4b1b7a3
oualdou <anas.oualdou@gmail.com>
2023-05-11 14:50:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,14 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-    
+      <Head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Depannage automobile en Ile-de-France ouvert 24h/24. Fiable, rapide et professionnel. Nous intervenons pour tous types de pannes, du depannage a la fourniture de carburant en urgence. Contactez-nous maintenant.">
+        <title>Dépannage Automobile en Île-de-France - Ouvert 24h/24</title>
+      </Head>
+
+     
 
       <Footer />
     </div>
