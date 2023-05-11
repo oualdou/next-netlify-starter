2bb68bca097847cce7e7556166836706371d5ebd
oualdou <anas.oualdou@gmail.com>
2023-05-11 16:36:18 +0200

    commit test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,14 @@ export default function Home() {
       </Head>
 
       <main>
-       
+      <Image
+        src="../public/background.png"
+        alt="Dépanneuse du future"
+        width={500}
+        height={300}
+      />
+     
+      
         <h1>Bienvenue sur notre site de dépannage automobile en Île-de-France, ouvert 24h/24 !</h1>
         <p>Lorsque vous êtes confronté à une panne de voiture, il est essentiel de pouvoir compter sur un service fiable et rapide. Notre équipe de professionnels expérimentés est là pour vous aider, que vous soyez en pleine nuit, en weekend ou pendant les jours fériés. Nous comprenons que les pannes de voiture peuvent survenir à tout moment, c est pourquoi nous sommes disponibles à toute heure pour vous fournir une assistance immédiate.</p>
         <p>Nos services de dépannage couvrent tous types de pannes, des problèmes de batterie déchargée aux crevaisons, en passant par les pannes mécaniques plus complexes. Nous disposons des équipements nécessaires pour résoudre la plupart des problèmes sur place, vous permettant ainsi de reprendre rapidement la route en toute tranquillité.</p>
@@ -22,6 +29,7 @@ export default function Home() {
         <p>Faites confiance à notre équipe compétente et dévouée pour prendre en charge votre véhicule avec professionnalisme et diligence. Nous sommes fiers de notre réputation en tant que prestataire de services de dépannage fiable en Île-de-France.</p>
         <p>N hésitez pas à nous contacter dès maintenant pour bénéficier de notre assistance rapide et efficace. Votre satisfaction est notre priorité absolue.</p>
       
+        <a href="tel:06.12.34.56.78">Appelez-nous directement : (+33) 06.12.34.56.78</a>
       </main>
 
       <Footer />
